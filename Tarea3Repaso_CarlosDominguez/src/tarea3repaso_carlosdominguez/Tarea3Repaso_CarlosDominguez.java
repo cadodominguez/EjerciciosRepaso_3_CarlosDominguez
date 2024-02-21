3de07bec5f261f190513abacd2e6a0145db486f6
cadodominguez <itzkarurosu@gmail.com>
2024-02-20 18:02:05 -0600

    base ya hecha

--- Tarea3Repaso_CarlosDominguez/src/tarea3repaso_carlosdominguez/Tarea3Repaso_CarlosDominguez.java
+++ Tarea3Repaso_CarlosDominguez/src/tarea3repaso_carlosdominguez/Tarea3Repaso_CarlosDominguez.java
@@ -1,20 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package tarea3repaso_carlosdominguez;
-
-/**
- *
- * @author USUARIO
- */
+import java.util.Scanner;
 public class Tarea3Repaso_CarlosDominguez {
-
-    /**
-     * @param args the command line arguments
-     */
+    
+    static Scanner brazil = new Scanner (System.in);
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        boolean out = true;
+        do {
+            System.out.println("**********MENU**********");
+            System.out.println("1.Veamos si podemos formar la palabra");
+            System.out.println("2.Numeros primero");
+            System.out.println("3.Ordenar alfabeticamente");
+            System.out.println("4.Codificar cadenas");
+            System.out.println("5.Jugando con arreglos");
+            System.out.println("6.Salir");
+            int opcion = brazil.nextInt();
+            switch (opcion) {
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                   
+                    break;
+                case 5:
+                    
+                   break;
+                case 6:
+                    System.out.println("Saliendo......");
+                    out = false;
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
+            } 
+        } while (out);
     }
     
 }
